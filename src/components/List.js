173cbab8fe71ef930cec469d9262cf7aecaf6c2d
saivishnu725 <saivishnu725@gmail.com>
2023-06-18 19:36:06 +0530

    <main> and Github

--- src/components/List.js
+++ src/components/List.js
@@ -1,20 +1,26 @@
 function List() {
   return (
-    <ul className="list">
-      <li> was first released in 2003 </li>
-      <li> was created by my parents </li>
-      <li> has well over 15 followers / 81 repositories (as of 18 June, 2023) on <a href="https://www.github.com/saivishnu725">Github</a> </li>
-      <li> is a Computer Science geek </li>
-      <li> 
-        hobbies:
-        <ul className="hobbies">
-          <li> Coding </li>
-          <li> Reading </li>
-          <li> Watching movies</li>
-          <li> Staring at the wall </li>
-        </ul>
-       </li>
-    </ul>
+    <main>
+      <ul className="list">
+        <li> was first released in 2003 </li>
+        <li> was created by my parents </li>
+        <li>
+          {" "}
+          has well over 15 followers / 81 repositories (as of 18 June, 2023) on{" "}
+          <a href="https://www.github.com/saivishnu725">Github</a>{" "}
+        </li>
+        <li> is a Computer Science geek </li>
+        <li>
+          hobbies:
+          <ul className="hobbies">
+            <li> Coding </li>
+            <li> Reading </li>
+            <li> Watching movies</li>
+            <li> Staring at the wall </li>
+          </ul>
+        </li>
+      </ul>
+    </main>
   );
 }
 
